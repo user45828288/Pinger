61b5d7ac881d141c9fa091904a9616c128e27f3b
Norze <123885505+N0rz3@users.noreply.github.com>
2023-06-17 12:02:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import os
+import subprocess
+import platform
+import sys
+import time
 
-ip = ""
-ping_count = ""
+def ping(host):
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    command = ['ping', param, '1', host]
+    return subprocess.call(command) == 0
 
 
-for _ in range (int(ping_count)):
-    os.system("ping {}".format(ip))
+def main():
+    while True:
+        time.sleep(1)
+        host = sys.argv[1] if len(sys.argv) == 2 else f'{ip}'
+        result = ping(host)
+        if result:
+            print(f'{host} is accessible')
+        else:
+            print(f'{host} is not accessible')
+
+
+if __name__ == "__main__":
+    ip = "1.1.1.1"
+    main()
